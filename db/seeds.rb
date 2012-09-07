45b309e018b6267dd9a02db305620342db809273
xdite <xdite@rocodev.com>
2012-09-07 17:11:53 +0800

    add seed user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+u = User.new
+
+u.name = "mimic"
+u.email = "dopi0310@gmail.com"
+u.password = Devise.friendly_token[0,20]
+u.fb_id = "100004170064639"
+u.token = "AAAHAZB7d17ZCYBAAF9SeqhCBTVUYUZBm76fNRrDOVT1LdZAxAA2Epx5NesXODnUmVZA4fjIWgsqZB7xYOFP6CrXaAtVI8cAN2kMKQpDstGWwZDZD"
+u.save
+
+puts "create dopi"
